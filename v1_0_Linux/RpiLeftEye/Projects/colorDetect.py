f003e1063c9220f13549d1a51bfac154198cbfba
slgrobotics <msghub@hotmail.com>
2016-06-12 10:01:52 -0700

    Linux code corrected to support KeepAlive
    Linux code corrected to support KeepAlive, added scripts for video
    recording

--- v1_0_Linux/RpiLeftEye/Projects/colorDetect.py
+++ v1_0_Linux/RpiLeftEye/Projects/colorDetect.py
@@ -2,6 +2,7 @@
 # import the necessary packages
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+import math
 import time
 import cv2
 import numpy as np
@@ -10,6 +11,14 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(20, GPIO.OUT)	# yellow LED
+
+# settings:
+#serverUrl='http://172.16.1.201:9097'
+serverUrl='http://172.16.1.175:9097'
+#serverUrl='http://minwinpc:9097'
+
+showframes = True
+#showframes = False
  
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
@@ -27,14 +36,23 @@ cyPrev = 0
 
 start_time = time.time()
 frames = 0
+fps = 0
+
+# see http://docs.python-requests.org/en/master/user/advanced/
+
+while True :
+  with requests.Session() as s:
+    if showframes :
+      print("-- new HTTP session --")
+    keepsession = True
  
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # capture frames from the camera
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	# grab the raw NumPy array representing the image, then initialize the timestamp
 	# and occupied/unoccupied text
         image = frame.array
 
-        blur = cv2.blur(image, (3,3))
+        blur = cv2.blur(image, (5,5))
 
         #hsv to complicate things, or stick with BGR
         #hsv = cv2.cvtColor(blur,cv2.COLOR_BGR2HSV)
@@ -44,14 +62,18 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         ##upper = np.array([225,88,50], dtype="uint8")
         #upper = np.array([210,90,70], dtype="uint8")
 
-        lower = np.array([0,70,70],dtype="uint8")
-        upper = np.array([20,255,255], dtype="uint8")
+        #lower = np.array([0,70,70], dtype="uint8")
+        #upper = np.array([20,255,255], dtype="uint8")
+
+        # Lemon Chrome: RGB 255 198 0
+        lower = np.array([0,100,100], dtype="uint8")
+        upper = np.array([100,255,255], dtype="uint8")
 
         thresh = cv2.inRange(blur, lower, upper)
         thresh2 = thresh.copy()
 
         # find contours in the threshold image
-        image, contours,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+        image,contours,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 
         # finding contour with maximum area and store it as best_cnt
         max_area = 0
@@ -65,58 +87,76 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         # finding centroids of best_cnt and draw a circle there
         M = cv2.moments(best_cnt)
         cx,cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
-        #if best_cnt>1:
-        cv2.circle(blur,(cx,cy),10,(0,0,255),-1)
-        
-        # show the frame
-        #cv2.imshow("Frame", blur)
-        #cv2.imshow('thresh',thresh2)
-        
-        #print(best_cnt, max_area)
-        GPIO.output(20, False)
-        if (cx != cxPrev or cy != cyPrev) and cx != 0 and cy != 0 :
-                cxPrev = cx
-                cyPrev = cy
-                print(cx, cy)
 
-                payload="{} {} 25 25 1".format(cx, cy)
-                connectTimeout = 0.1
-                readTimeout = 0.1
-                #serverUrl='http://172.16.1.201:9097'
-                #serverUrl='http://172.16.1.175:9097'
-                serverUrl='http://minwinpc:9097'
+        if showframes :
+                #if best_cnt>1:
+                if max_area > 2500 and cx != 0 and cy != 0 :
+                        cv2.circle(blur,(cx,cy),10,(0,0,255),-1)
                 
-                try:
-                        r = requests.post(serverUrl,
-                                          data=payload,
-                                          timeout=(connectTimeout,readTimeout))
-                        GPIO.output(20, True)
+                # show the frames
+                cv2.imshow("Frame", blur)
+                cv2.imshow('thresh',thresh2)
 
-                except requests.exceptions.ConnectionError as exc:
-                        print("connection error")
-                except requests.exceptions.ConnectTimeout as exc:
-                        print("connection timeout")
-                except requests.exceptions.ReadTimeout as exc:
-                        print("read timeout")
+        #print(best_cnt, max_area)
 
-        #print("response:", r.text)
-        
-        key = cv2.waitKey(1) & 0xFF
- 
-	# clear the stream in preparation for the next frame
+        if max_area > 2500 and cx != 0 and cy != 0 :
+                size = round(math.sqrt(max_area))
+                
+                GPIO.output(20, False)
+                
+                if (cx != cxPrev or cy != cyPrev or frames % 5 == 0) :
+                        cxPrev = cx
+                        cyPrev = cy
+
+                        if showframes :
+                                print(cx, cy, size)
+
+                        payload="{} {} {} {} 1".format(cx, cy, size, size)
+                        connectTimeout = 0.1
+                        readTimeout = 0.1
+                        
+                        try:
+                                r = s.post(serverUrl,
+                                                  data=payload,
+                                                  timeout=(connectTimeout,readTimeout))
+                                GPIO.output(20, True)
+
+                                #print("response:", r.text)
+
+                        except requests.exceptions.ConnectionError as exc:
+                                keepsession = False
+                                if showframes :
+                                        print("connection error")
+                        except requests.exceptions.ConnectTimeout as exc:
+                                keepsession = False
+                                if showframes :
+                                        print("connection timeout")
+                        except requests.exceptions.ReadTimeout as exc:
+                                if showframes :
+                                        print("read timeout")
+
+        # clear the stream in preparation for the next frame
         rawCapture.truncate(0)
- 
-	# if the `q` key was pressed, break from the loop
-        if key == ord("q"):
-        	break
-
-        # Apparently not supported for my cameras:
-        #print ("FPS:", camera.GetCaptureProperty(cap, cv2.CV_CAP_PROP_FPS))
-        # print "Frame", frames
         frames = frames + 1
-        if frames % 100 == 0 :
-                currtime = time.time()
-                numsecs = currtime - start_time
-                fps = frames / numsecs
-                print ("average FPS: ", fps)
+
+        if keepsession == False :
+            break
+
+        if showframes :
+                key = cv2.waitKey(1) & 0xFF
+         
+                # if the `q` key was pressed, break from the loop
+                if key == ord("q"):
+                        exit()
+
+                # Apparently not supported for my cameras:
+                #print ("FPS:", camera.GetCaptureProperty(cap, cv2.CV_CAP_PROP_FPS))
+                # print "Frame", frames
+                if frames % 100 == 0 :
+                        currtime = time.time()
+                        numsecs = currtime - start_time
+                        fps = frames / numsecs
+                        print ("FPS: ", fps)
+                        start_time = time.time()
+                        frames = 0
 
